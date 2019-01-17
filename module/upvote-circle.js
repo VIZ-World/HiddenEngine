276380ebc64d6b968ac1238e3642454372f12508
Anatoly Piskunov <anatoly.piskunov@gmail.com>
2019-01-17 12:26:06 +0000

    Rework upvote-circle, remove golos/steem, remove flag

--- module/upvote-circle.js
+++ module/upvote-circle.js
@@ -23,52 +23,16 @@ module.exports=class he_module{
 				if(_this.session.auth){
 					if(typeof _this._POST.add_vote_action !== 'undefined'){
 						for(var account of global.he.accounts){
-							if(account.type==parseInt(_this._POST.type)){
-								if(typeof account.upvote_circle !== 'undefined'){
-									if(1==account.upvote_circle){
-										if(typeof account.posting_key !== 'undefined'){
-											if(''!=account.posting_key){
-												var queue_item={'action':'vote','data':{'user_login':account.login,'user_posting_key':account.posting_key,'target_login':_this._POST.target_login,'target_permlink':_this._POST.target_permlink,'vote_weight':parseInt(_this._POST.vote_weight)}};
-												if(parseInt(_this._POST.action_delay)>0){
-													queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
-												}											if(1==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.steem_queue;
-													global.he.steem_queue.push(queue_item);
-												}
-												if(2==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.golos_queue;
-													global.he.golos_queue.push(queue_item);
-												}
+							if(typeof account.upvote_circle !== 'undefined'){
+								if(1==account.upvote_circle){
+									if(typeof account.posting_key !== 'undefined'){
+										if(''!=account.posting_key){
+											var queue_item={'action':'vote','data':{'user_login':account.login,'user_posting_key':account.posting_key,'target_login':_this._POST.target_login,'target_permlink':_this._POST.target_permlink,'vote_weight':parseInt(_this._POST.vote_weight)}};
+											if(parseInt(_this._POST.action_delay)>0){
+												queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
 											}
-										}
-									}
-								}
-							}
-						}
-						_this.session.redirect=true;
-						_this.response.redirect(302,'/upvote-circle/');
-					}
-					else
-					if(typeof _this._POST.add_flag_action !== 'undefined'){
-						for(var account of global.he.accounts){
-							if(account.type==parseInt(_this._POST.type)){
-								if(typeof account.upvote_circle !== 'undefined'){
-									if(1==account.upvote_circle){
-										if(typeof account.posting_key !== 'undefined'){
-											if(''!=account.posting_key){
-												var queue_item={'action':'flag','data':{'user_login':account.login,'user_posting_key':account.posting_key,'target_login':_this._POST.target_login,'target_permlink':_this._POST.target_permlink,'vote_weight':parseInt(_this._POST.vote_weight)}};
-												if(parseInt(_this._POST.action_delay)>0){
-													queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
-												}
-												if(1==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.steem_queue;
-													global.he.steem_queue.push(queue_item);
-												}
-												if(2==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.golos_queue;
-													global.he.golos_queue.push(queue_item);
-												}
-											}
+											queue_item.id=++global.he.counters.queue;
+											global.he.queue.push(queue_item);
 										}
 									}
 								}
@@ -80,24 +44,16 @@ module.exports=class he_module{
 					else
 					if(typeof _this._POST.add_witness_vote_action !== 'undefined'){
 						for(var account of global.he.accounts){
-							if(account.type==parseInt(_this._POST.type)){
-								if(typeof account.upvote_circle !== 'undefined'){
-									if(1==account.upvote_circle){
-										if(typeof account.active_key !== 'undefined'){
-											if(''!=account.active_key){
-												var queue_item={'action':'witness_vote','data':{'user_login':account.login,'user_active_key':account.active_key,'target_login':_this._POST.target_login}};
-												if(parseInt(_this._POST.action_delay)>0){
-													queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
-												}
-												if(1==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.steem_queue;
-													global.he.steem_queue.push(queue_item);
-												}
-												if(2==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.golos_queue;
-													global.he.golos_queue.push(queue_item);
-												}
+							if(typeof account.upvote_circle !== 'undefined'){
+								if(1==account.upvote_circle){
+									if(typeof account.active_key !== 'undefined'){
+										if(''!=account.active_key){
+											var queue_item={'action':'witness_vote','data':{'user_login':account.login,'user_active_key':account.active_key,'target_login':_this._POST.target_login}};
+											if(parseInt(_this._POST.action_delay)>0){
+												queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
 											}
+											queue_item.id=++global.he.counters.queue;
+											global.he.queue.push(queue_item);
 										}
 									}
 								}
@@ -109,24 +65,16 @@ module.exports=class he_module{
 					else
 					if(typeof _this._POST.add_witness_unvote_action !== 'undefined'){
 						for(var account of global.he.accounts){
-							if(account.type==parseInt(_this._POST.type)){
-								if(typeof account.upvote_circle !== 'undefined'){
-									if(1==account.upvote_circle){
-										if(typeof account.active_key !== 'undefined'){
-											if(''!=account.active_key){
-												var queue_item={'action':'witness_unvote','data':{'user_login':account.login,'user_active_key':account.active_key,'target_login':_this._POST.target_login}};
-												if(parseInt(_this._POST.action_delay)>0){
-													queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
-												}
-												if(1==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.steem_queue;
-													global.he.steem_queue.push(queue_item);
-												}
-												if(2==parseInt(_this._POST.type)){
-													queue_item.id=++global.he.counters.golos_queue;
-													global.he.golos_queue.push(queue_item);
-												}
+							if(typeof account.upvote_circle !== 'undefined'){
+								if(1==account.upvote_circle){
+									if(typeof account.active_key !== 'undefined'){
+										if(''!=account.active_key){
+											var queue_item={'action':'witness_unvote','data':{'user_login':account.login,'user_active_key':account.active_key,'target_login':_this._POST.target_login}};
+											if(parseInt(_this._POST.action_delay)>0){
+												queue_item.time=new Date().getTime() + (parseInt(_this._POST.action_delay)*1000);
 											}
+											queue_item.id=++global.he.counters.queue;
+											global.he.queue.push(queue_item);
 										}
 									}
 								}
@@ -180,11 +128,6 @@ module.exports=class he_module{
 						_this.content+=`<h1>Upvote circle list</h1>`;
 						_this.content+=`<h2>Adding vote action</h2>`;
 						_this.content+=`<form action="" method="POST">
-						Blockchain:<br>
-						<select name="type">
-							<option value="1">Steem</option>
-							<option value="2" selected>Golos</option>
-						</select>
 						<br>Target login:<br><input type="text" name="target_login" value="">
 						<br>Target permlink:<br><input type="text" name="target_permlink" value="">
 						<br>Vote weight:<br><input type="text" name="vote_weight" value="10000">
@@ -197,11 +140,6 @@ module.exports=class he_module{
 						_this.content+=`<h1>Upvote circle list</h1>`;
 						_this.content+=`<h2>Adding flag action</h2>`;
 						_this.content+=`<form action="" method="POST">
-						Blockchain:<br>
-						<select name="type">
-							<option value="1">Steem</option>
-							<option value="2" selected>Golos</option>
-						</select>
 						<br>Target login:<br><input type="text" name="target_login" value="">
 						<br>Target permlink:<br><input type="text" name="target_permlink" value="">
 						<br>Flag weight:<br><input type="text" name="vote_weight" value="10000">
@@ -214,11 +152,6 @@ module.exports=class he_module{
 						_this.content+=`<h1>Upvote circle list</h1>`;
 						_this.content+=`<h2>Adding witness vote action</h2>`;
 						_this.content+=`<form action="" method="POST">
-						Blockchain:<br>
-						<select name="type">
-							<option value="1">Steem</option>
-							<option value="2" selected>Golos</option>
-						</select>
 						<br>Target login:<br><input type="text" name="target_login" value="">
 						<br>Delay:<br><input type="text" name="action_delay" value="" placeholder="seconds">
 						<hr><input type="submit" class="button" name="add_witness_vote_action" value="Submit">
@@ -229,11 +162,6 @@ module.exports=class he_module{
 						_this.content+=`<h1>Upvote circle list</h1>`;
 						_this.content+=`<h2>Adding witness unvote action</h2>`;
 						_this.content+=`<form action="" method="POST">
-						Blockchain:<br>
-						<select name="type">
-							<option value="1">Steem</option>
-							<option value="2" selected>Golos</option>
-						</select>
 						<br>Target login:<br><input type="text" name="target_login" value="">
 						<br>Delay:<br><input type="text" name="action_delay" value="" placeholder="seconds">
 						<hr><input type="submit" class="button" name="add_witness_unvote_action" value="Submit">
@@ -253,9 +181,8 @@ module.exports=class he_module{
 						_this.content+=`<p></p>`;
 						_this.content+=`<table class="object_view"><thead><tr>
 							<th width="5%">id</th>
-							<th width="5%">Type</th>
 							<th width="25%">Login</th>
-							<th width="25%">Status</th>
+							<th width="30%">Status</th>
 							<th width="40%">Actions</th>
 						</tr>
 						</thead>
@@ -264,7 +191,6 @@ module.exports=class he_module{
 							_this.content+=`
 						<tr>
 							<td>${m.id}</td>
-							<td>`+(1==m.type?'STEEM':'GOLOS')+`</td>
 							<td>${m.login}</td>`;
 							if(1==m.upvote_circle){
 								_this.content+=`
